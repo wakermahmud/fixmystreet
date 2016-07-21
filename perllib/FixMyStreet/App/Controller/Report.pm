bd5d9447be95885dbb3f001280d5c2db6eb52e0d
Dave Arter <davea@mysociety.org>
2016-07-21 17:06:10 +0100

    Show available categories in dropdown on inspector page

--- perllib/FixMyStreet/App/Controller/Report.pm
+++ perllib/FixMyStreet/App/Controller/Report.pm
@@ -300,6 +300,7 @@ sub inspect : Private {
     $c->forward('/auth/get_csrf_token');
     $c->forward( 'load_problem_or_display_error', [ $id ] );
     $c->forward( 'check_has_permission_to', [ 'report_inspect' ] );
+    $c->stash->{categories} = $c->forward('/admin/categories_for_point');
     $c->forward( 'format_problem_for_display' );
 };
 
